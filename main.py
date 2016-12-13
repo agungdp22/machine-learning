d47199c933b35a32b318800902ac0b8726d55fa3
agungdp22 <agungdwiprasetyo22@gmail.com>
2016-12-14 04:40:17 +0700

    edit class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from machineLearning import LVQ
+from metode.LVQ import LVQ
 import numpy as np
 
 data = np.loadtxt("data.txt", usecols=[0,1,2,3,4])
@@ -9,10 +9,6 @@ lvq = LVQ(data)
 lvq.learning()
 lvq.printBobot()
 
-tes = []
-print "Masukkan data tes:"
-for i in range(4):
-	inp = int(input())
-	tes.append(inp)
-
+print "Masukkan data tes (ex. 1 1 0 1):"
+tes = map(int, raw_input().split())
 lvq.prediksi(tes)
